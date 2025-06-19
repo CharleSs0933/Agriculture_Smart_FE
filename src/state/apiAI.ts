0a96aec8af96572d017b3b09019c89c698f1ae70
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2025-06-19 20:37:27 +0700

    URL AI

--- src/state/apiAI.ts
+++ src/state/apiAI.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiAI = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://4e3b-171-243-49-47.ngrok-free.app",
+    baseUrl: "https://8f4a-171-243-49-150.ngrok-free.app",
     prepareHeaders: (headers) => {
       return headers;
     },
